6ba0268fa15f9b8d2d6adcdcd1484daa21b17a4c
Prem Sichanugrist <s@sikachu.com>
2011-08-18 21:37:07 +0700

    Move the ENV config into the support/env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,3 +6,9 @@ World(Test::Unit::Assertions)
 Before do
   @aruba_timeout_seconds = 30
 end
+
+if ENV['TRAVIS']
+  ENV["AWS_ACCESS_KEY_ID"] = "AKIAI2Q4BG4ALE7GOTIA"
+  ENV["AWS_SECRET_ACCESS_KEY"] = "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"
+  ENV["PAPERCLIP_TEST_BUCKET"] = "paperclip-travis"
+end
