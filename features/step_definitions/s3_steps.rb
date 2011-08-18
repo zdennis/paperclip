47a23f584b395a61b5b6b70ade54bd6119129fd1
Prem Sichanugrist <s@sikachu.com>
2011-08-18 10:57:29 +0700

    Use `Bucket#objects` instead of `Service.buckets` so the plugin doesn't require the `ListAllMyBuckets` permission

--- features/step_definitions/s3_steps.rb
+++ features/step_definitions/s3_steps.rb
@@ -1,9 +1,11 @@
 Given /I validate my S3 credentials/ do
   key = ENV['AWS_ACCESS_KEY_ID']
   secret = ENV['AWS_SECRET_ACCESS_KEY']
+  bucket = ENV['PAPERCLIP_TEST_BUCKET']
 
   key.should_not be_nil
   secret.should_not be_nil
+  bucket.should_not be_nil
 
-  assert_credentials(key, secret)
+  assert_credentials(key, secret, bucket)
 end
--- features/support/s3.rb
+++ features/support/s3.rb
@@ -7,14 +7,14 @@ module AWSS3Methods
     end
   end
 
-  def assert_credentials(key, secret)
+  def assert_credentials(key, secret, bucket)
     load_s3
     begin
       AWS::S3::Base.establish_connection!(
         :access_key_id => key,
         :secret_access_key => secret
       )
-      AWS::S3::Service.buckets
+      AWS::S3::Bucket.objects(bucket, :max_keys => 0)
     rescue AWS::S3::ResponseError => e
       fail "Could not connect using AWS credentials in AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY. " +
            "Please make sure these are set in your environment."
