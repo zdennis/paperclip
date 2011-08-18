b50ee32e8b1366252a9abbe02641cad7f1cc8107
Prem Sichanugrist <s@sikachu.com>
2011-08-18 14:29:41 +0700

    Use the token instead of hardcoding the bucket name in case the bucket name has changed

--- features/step_definitions/html_steps.rb
+++ features/step_definitions/html_steps.rb
@@ -1,10 +1,12 @@
 Then %r{I should see an image with a path of "([^"]*)"} do |path|
+  path = path.gsub(/%{BUCKET_NAME}/, ENV['PAPERCLIP_TEST_BUCKET'] || 'paperclip')
   page.should have_css("img[src^='#{path}']")
 end
 
 Then %r{^the file at "([^"]*)" is the same as "([^"]*)"$} do |web_file, path|
   expected = IO.read(path)
   actual = if web_file.match %r{^https?://}
+    web_file = web_file.gsub(/%{BUCKET_NAME}/, ENV['PAPERCLIP_TEST_BUCKET'] || 'paperclip')
     Net::HTTP.get(URI.parse(web_file))
   else
     visit(web_file)
