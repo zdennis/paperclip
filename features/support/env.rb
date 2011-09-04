4e11fdd12669ec4d32a2b1c52c48b52b42c1f498
Prem Sichanugrist <s@sikachu.com>
2011-09-04 22:56:14 +0700

    Debug the current time of server

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,6 +9,7 @@ end
 
 if ENV['TRAVIS']
   puts "Using Travis CI configuration"
+  puts "Current time is #{`date`} : #{Time.now}"
   ENV["AWS_ACCESS_KEY_ID"] = "AKIAI2Q4BG4ALE7GOTIA"
   ENV["AWS_SECRET_ACCESS_KEY"] = "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"
   ENV["PAPERCLIP_TEST_BUCKET"] = "paperclip-travis"
