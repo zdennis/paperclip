02c8fa2a6b03006e8c9f78a26ad1036789a6f161
Prem Sichanugrist <s@sikachu.com>
2011-09-04 22:36:59 +0700

    Let's output if we're using Travis CI configuration or not.

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,6 +8,7 @@ Before do
 end
 
 if ENV['TRAVIS']
+  puts "Using Travis CI configuration"
   ENV["AWS_ACCESS_KEY_ID"] = "AKIAI2Q4BG4ALE7GOTIA"
   ENV["AWS_SECRET_ACCESS_KEY"] = "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"
   ENV["PAPERCLIP_TEST_BUCKET"] = "paperclip-travis"
